9c15f008e3cbccf32d3cf9272aad69aa9708dfd0
Xiaba2203 <xevaxevatita@gmail.com>
2025-09-17 10:44:16 -0500

    Add files via upload

---
+++ lab-progra-avanzada/lab2/Chavarria74032240/P4.py
@@ -0,0 +1,6 @@
+t = (1, 2, 3, 7, 9, 0, 5)  # tupla ejemplo
+
+print(max(t))  # imprime el mayor
+print(min(t))  # imprime el menor
+print(sum(t))  # imprime la suma
+print(len(t))  # imprime cantidad de elementos
